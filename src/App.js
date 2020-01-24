8d2406e810a817e67ea14855f1f07731181ebd33
Dylan Hall <dehall@mitre.org>
2020-01-24 16:49:25 -0500

    update to use fhir-visualizers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { PatientVisualizer, ConditionsVisualizer, ObservationsVisualizer } from './Visualizers';
+import { PatientVisualizer, ConditionsVisualizer, ObservationsVisualizer } from 'fhir-visualizers';
+
+import bundle from './demo_data.json';
+
 
 export default class App extends React.Component {
 
   render() {
+  	const allResources = bundle.entry.map(e => e.resource);
+  	const patient = allResources.find(r => r.resourceType === 'Patient');
+  	const conditions = allResources.filter(r => r.resourceType === 'Condition');
+  	const observations = allResources.filter(r => r.resourceType === 'Observation');
+
     return (
       <div>
-        <PatientVisualizer />
-        <ConditionsVisualizer />
-        <ObservationsVisualizer />
+        <PatientVisualizer patient={patient} />
+        <ConditionsVisualizer rows={conditions}/>
+        <ObservationsVisualizer rows={observations} />
       </div>
     );
   }
