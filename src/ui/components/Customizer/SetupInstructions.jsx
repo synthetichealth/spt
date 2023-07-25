bf9f36cd57e09c4d1a11238432b4e92865a9d18b
Dylan Hall <dehall@mitre.org>
2023-07-25 14:45:44 -0400

    add more descriptive text

--- src/ui/components/Customizer/SetupInstructions.jsx
+++ src/ui/components/Customizer/SetupInstructions.jsx
@@ -52,7 +52,7 @@ const BasicSetupInstructions = props => {
      (ability to navigate between folders and run commands).<br/>
     Synthea requires the Java™ JDK 11 or newer to be installed 
     (make sure to select the JDK, not the JRE install).
-    We recommend the prebuilt OpenJDK binaries available from 
+    We recommend the prebuilt OpenJDK binaries available from&nbsp;
     <a href="https://adoptium.net/" target="_blank">https://adoptium.net/</a>.
 <br /><br />
 <h5>Setup</h5>
@@ -73,7 +73,35 @@ const BasicSetupInstructions = props => {
     <br/>
     <BashCodeBlock code={renderArgs('java -jar synthea-with-dependencies.jar', myArgs, config)} />
     <br />
+    When you run this command, you should see output similar to the following:
+    <pre>
+    Scanned 60 modules and 36 submodules.<br/>
+    Loading submodule modules/breast_cancer/tnm_diagnosis.json<br/>
+    Loading submodule modules/allergies/allergy_incidence.json<br/>
+    Loading submodule modules/dermatitis/moderate_cd_obs.json<br/>
+    ...<br/>
+    Loading module modules/opioid_addiction.json<br/>
+    Loading module modules/dialysis.json<br/>
+    ...<br/>
+    Loading module modules/hypertension.json<br/>
+    Running with options:<br/>
+    Population: 1<br/>
+    Seed: 1570658792125<br/>
+    Provider Seed:1570658792125<br/>
+    Location: Massachusetts<br/>
+    Min Age: 0<br/>
+    Max Age: 140<br/>
+    1 -- Arthur650 Carroll471 (39 y/o M) Southwick, Massachusetts <br/>
+    {"{alive=1, dead=0}"}
+    </pre>
+    <br/>
+    Once the process completes, you will see an <code>output</code> folder
+    alongside the <code>synthea-with-dependencies.jar</code>, 
+    and a folder inside the <code>output</code> folder for each output format selected. 
 
+    You can review these files in your text editor of choice, 
+    or use the <a href="#/record_viewer" target="_blank">Patient Viewer</a> 
+    to quickly view the contents of a FHIR JSON file.
 </div>);
 }
 
@@ -128,7 +156,31 @@ To run Synthea with your desired settings, use the <code>run_synthea</code> scri
 <br /><br />
 
 
-When you run this command, you should see output similar to the following:</div>);
+When you run this command, you should see output similar to the following:
+<pre>
+&gt; Task :run<br/>
+Loading modules\allergic_rhinitis.json<br/>
+Loading modules\allergies\allergy_incidence.json<br/>
+[... many more lines of Loading ...]<br/>
+Loading modules\wellness_encounters.json<br/>
+Loaded 68 modules.<br/>
+Running with options:<br/>
+Population: 1<br/>
+Seed: 1519063214833<br/>
+Location: Massachusetts<br/>
+<br/>
+1 -- Jerilyn993 Parker433 (10 y/o) Lawrence, Massachusetts<br/>
+</pre>
+
+Once the process completes, you will see an <code>output</code> folder
+inside the <code>synthea</code> folder, 
+and a folder inside the <code>output</code> folder for each output format selected. 
+
+You can review these files in your text editor of choice, 
+or use the <a href="#/record_viewer" target="_blank">Patient Viewer</a> 
+to quickly view the contents of a FHIR JSON file.
+
+</div>);
 }
 
 // For Docker, everything is packaged together into just the dockerfile
