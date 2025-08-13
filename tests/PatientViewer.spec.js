f9d5e6520034c0d35a484d807b7e38529bbb9422
Shaumik-Ashraf <shaumikashraf@mitre.org>
2025-08-13 13:25:26 -0400

    fix PatientViewer async bug

--- tests/PatientViewer.spec.js
+++ tests/PatientViewer.spec.js
@@ -1,3 +1,4 @@
+
 import { test, expect, describe, beforeEach } from '@playwright/test';
 
 const fs = require("fs");
@@ -5,13 +6,16 @@ const path = require("path");
 
 const BUNDLE_FIXTURE = 'fixtures/output/fhir/Bo157_Schroeder447_013ba640-77dd-1136-b35e-2ff0b3c48ff5.json';
 var bundle = JSON.parse(fs.readFileSync(path.resolve(__dirname, BUNDLE_FIXTURE), 'utf-8'));
+var patient = bundle.entry[0].resource;
 
 describe('PatientViewer', () => {
   beforeEach(async ({ page }) => {
-    await page.goto('http://127.0.0.1:3000/spt/#/record_viewer'); 
+    // go to root first to reset app
+    await page.goto('http://127.0.0.1:3000/');
+    await page.goto('http://127.0.0.1:3000/spt/#/record_viewer');
 
     await page.locator('input[type="file"]').setInputFiles(path.resolve(__dirname, BUNDLE_FIXTURE));
-    await page.waitForTimeout(5000); 
+    await page.waitForTimeout(1000);
   });
 
   test('renders and uploads FHIR bundle successfully', async ({ page }) => {
@@ -19,19 +23,37 @@ describe('PatientViewer', () => {
   });
 
   describe('patient section', () => {
-    test('renders patient basic information', async ({ page }) => {
-      var patient = bundle.entry[0].resource;
-      var patient_section = page.locator('[data-test-id="patient"]');
+    // beforeEach(async ({ page }) => {
+    // });
+    
+    // DEBUGGING
+    test('renders patient heading', async ({ page }) => {
+      const patient_section = page.locator('[data-test-id="patient"]');
+      //await patient_section.waitFor({ state: "attached" });
+      //console.log(patient_section.innerHTML());
+      await expect(patient_section).toContainText("Patient", {ignoreCase: true, useInnerText: true});
+    });
 
+    test('renders patient name', async ({ page }) => {
+      const patient_section = page.locator('[data-test-id="patient"]');
       await expect(patient_section).toContainText(patient.name[0].family);
-      patient.name[0].given.forEach(async (given_name) => {
+      for(const given_name of patient.name[0].given) {
         await expect(patient_section).toContainText(given_name);
-      });
+      }
+//      await expect(patient_section).toContainText(patient.name[0].given[0]);
+    });
 
-      expect(patient_section).toContainText(patient.gender);
+    test('renders patient gender', async ({ page }) => {
+      const patient_section = page.locator('[data-test-id="patient"]');
+      await expect(patient_section).toContainText(patient.gender);
+    });
 
-      expect(patient_section).toContainText(patient.address[0].line);
-      expect(patient_section).toContainText(patient.address[0].city);
+    test('renders patient address', async ({ page }) => {
+      const patient_section = page.locator('[data-test-id="patient"]');
+      await expect(patient_section).toContainText(patient.address[0].line[0]);
+      await expect(patient_section).toContainText(patient.address[0].city);
+      await expect(patient_section).toContainText(patient.address[0].state);
+      await expect(patient_section).toContainText(patient.address[0].postalCode);
     });
   });
 
